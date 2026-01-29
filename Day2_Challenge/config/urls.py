0961b650a9850b0a117bf325e0a25f7372c9162f
Justman-yzz <a01088744395@gmail.com>
2026-01-29 11:36:34 +0900

    ✨ Feat: todo URL 라우팅 추가

--- Day2_Challenge/config/urls.py
+++ Day2_Challenge/config/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from todo.views import todo_list, todo_info # 추가시키기
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('todo/', todo_list, name='todo_list'),
+    path('todo/<int:todo_id>/', todo_info, name='todo_info'),
 ]
